5bb749cb91d4a93509caef161a9396163f6f5fc4
Himanshu Gupta <himanshu@knoldus.com>
2017-12-30 11:38:48 +0530

    improved cassandra table initialization code

--- src/main/scala/knolx/StructuredStreamingWordCount.scala
+++ src/main/scala/knolx/StructuredStreamingWordCount.scala
@@ -1,21 +1,19 @@
 package knolx
 
-import com.datastax.driver.core.{Cluster, Session}
+import com.datastax.driver.core.Cluster
 import knolx.Config.{cassandraHosts, keyspace}
 
 /**
   * Copyright Knoldus Software LLP. All rights reserved.
   */
 object StructuredStreamingWordCount extends App {
-  var session: Session = _
-  try {
-    val cluster = Cluster.builder.addContactPoints(cassandraHosts).build
-    cluster.newSession().execute(s"CREATE KEYSPACE IF NOT EXISTS $keyspace WITH replication = {'class':'SimpleStrategy','replication_factor':1};")
+  val cluster = Cluster.builder.addContactPoints(cassandraHosts).build
+  val session = cluster.newSession()
 
-    session = cluster.connect(keyspace)
-    session.execute("CREATE TABLE IF NOT EXISTS wordcount ( word text PRIMARY KEY,count int );")
-  } finally {
-    session.close()
-    session.getCluster.close()
-  }
+  session.execute(s"CREATE KEYSPACE IF NOT EXISTS $keyspace WITH " +
+    "replication = {'class':'SimpleStrategy','replication_factor':1};")
+
+  session.execute(s"CREATE TABLE IF NOT EXISTS $keyspace.wordcount ( word text PRIMARY KEY,count int );")
+  session.close()
+  session.getCluster.close()
 }
