6585bb63b455b3c726df8966b6669124b1fdff96
Himanshu Gupta <himanshu@knoldus.com>
2017-12-30 13:09:33 +0530

    created spark session

--- src/main/scala/knolx/Config.scala
+++ src/main/scala/knolx/Config.scala
@@ -8,4 +8,6 @@ object Config {
   val topic = System.getenv("TOPIC")
   val cassandraHosts = System.getenv("CASSANDRA_HOSTS")
   val keyspace = System.getenv("CASSANDRA_KEYSPACE")
+  val sparkMaster = System.getenv("SPARK_MASTER")
+  val sparkAppName = System.getenv("SPARK_APP_NAME")
 }
--- src/main/scala/knolx/StructuredStreamingWordCount.scala
+++ src/main/scala/knolx/StructuredStreamingWordCount.scala
@@ -1,7 +1,8 @@
 package knolx
 
 import com.datastax.driver.core.Cluster
-import knolx.Config.{cassandraHosts, keyspace}
+import knolx.Config.{cassandraHosts, keyspace, sparkAppName, sparkMaster}
+import org.apache.spark.sql.SparkSession
 
 /**
   * Copyright Knoldus Software LLP. All rights reserved.
@@ -10,11 +11,16 @@ object StructuredStreamingWordCount extends App with KnolXLogger {
   val cluster = Cluster.builder.addContactPoints(cassandraHosts).build
   val session = cluster.newSession()
 
-  info("Creating Cassandra Keypsace and tables")
+  info("Creating Keypsace and tables in Cassandra")
   session.execute(s"CREATE KEYSPACE IF NOT EXISTS $keyspace WITH " +
-    "replication = {'class':'SimpleStrategy','replication_factor':1};")
+  "replication = {'class':'SimpleStrategy','replication_factor':1};")
 
   session.execute(s"CREATE TABLE IF NOT EXISTS $keyspace.wordcount ( word text PRIMARY KEY,count int );")
+
+  info("Closing DB connection")
   session.close()
   session.getCluster.close()
+
+  info("Creating Spark Session")
+  val spark = SparkSession.builder().master(sparkMaster).appName(sparkAppName).getOrCreate()
 }
