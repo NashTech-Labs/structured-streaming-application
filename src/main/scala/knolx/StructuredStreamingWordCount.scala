16ca8805395b1d032c69dab19d7e418e26ec4d25
Himanshu Gupta <himanshu@knoldus.com>
2017-12-30 14:27:47 +0530

    created streaming query for word count but only for console format

--- src/main/scala/knolx/StructuredStreamingWordCount.scala
+++ src/main/scala/knolx/StructuredStreamingWordCount.scala
@@ -1,8 +1,11 @@
 package knolx
 
 import com.datastax.driver.core.Cluster
-import knolx.Config.{cassandraHosts, keyspace, sparkAppName, sparkMaster}
+import knolx.Config._
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, lit, sum}
+import org.apache.spark.sql.streaming.OutputMode
+import org.apache.spark.sql.types.StringType
 
 /**
   * Copyright Knoldus Software LLP. All rights reserved.
@@ -13,7 +16,7 @@ object StructuredStreamingWordCount extends App with KnolXLogger {
 
   info("Creating Keypsace and tables in Cassandra")
   session.execute(s"CREATE KEYSPACE IF NOT EXISTS $keyspace WITH " +
-  "replication = {'class':'SimpleStrategy','replication_factor':1};")
+    "replication = {'class':'SimpleStrategy','replication_factor':1};")
 
   session.execute(s"CREATE TABLE IF NOT EXISTS $keyspace.wordcount ( word text PRIMARY KEY,count int );")
 
@@ -23,4 +26,30 @@ object StructuredStreamingWordCount extends App with KnolXLogger {
 
   info("Creating Spark Session")
   val spark = SparkSession.builder().master(sparkMaster).appName(sparkAppName).getOrCreate()
+  spark.sparkContext.setLogLevel("WARN")
+
+  info("Creating Streaming DF")
+  val dataStream =
+    spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", bootstrapServer)
+      .option("subscribe", topic)
+      .load()
+
+  info("Starting Streaming Query")
+  val query =
+    dataStream
+      .select(col("value").cast(StringType).as("word"), lit(1).as("count"))
+      .groupBy(col("word"))
+      .agg(sum("count").as("count"))
+      .writeStream
+      .format("console")
+      .outputMode(OutputMode.Update())
+      .option("truncate", false)
+      .start()
+
+  info("Waiting for the query to terminate")
+  query.awaitTermination()
+  query.stop()
 }
