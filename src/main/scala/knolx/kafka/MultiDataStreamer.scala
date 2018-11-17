a9c9c83e5253c438d80b378c97fa6807dd53ee0e
Himanshu Gupta <himanshu@knoldus.com>
2018-11-18 01:11:06 +0530

    prepared tech triveni demo

--- src/main/scala/knolx/kafka/MultiDataStreamer.scala
+++ src/main/scala/knolx/kafka/MultiDataStreamer.scala
@@ -5,21 +5,18 @@ import java.util.Properties
 import akka.actor.ActorSystem
 import knolx.Config.{bootstrapServer, topic}
 import knolx.KnolXLogger
-import knolx.spark.Device
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
 import org.apache.kafka.common.serialization.StringSerializer
-import org.json4s.DefaultFormats
-import org.json4s.jackson.Serialization.write
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
+import scala.language.postfixOps
+import scala.util.Random
 
 /**
   * Copyright Knoldus Inc.. All rights reserved.
   */
 object MultiDataStreamer extends App with KnolXLogger {
-  private implicit val formats: DefaultFormats.type = DefaultFormats
-
   val system = ActorSystem("DataStreamer")
 
   val props = new Properties()
@@ -31,9 +28,8 @@ object MultiDataStreamer extends App with KnolXLogger {
 
   info("Streaming data into Kafka...")
   system.scheduler.schedule(0 seconds, 3000 milliseconds) {
-    (1 to 100).foreach { id =>
-      val deviceData = Device(s"device$id", Math.random * 49 + 1)
-      producer.send(new ProducerRecord[String, String](topic, write(deviceData)))
+    (1 to Random.nextInt(100)).foreach { id =>
+      producer.send(new ProducerRecord[String, String](topic,s"device$id", (Math.random * 2 + 1).toString))
     }
   }
 }
---
+++ src/main/scala/knolx/spark/MultiStreamHandler.scala
@@ -0,0 +1,87 @@
+package knolx.spark
+
+import knolx.Config._
+import knolx.KnolXLogger
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.streaming.{GroupState, GroupStateTimeout, OutputMode}
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.{Encoders, SparkSession}
+
+/**
+  * Copyright Knoldus, Inc. 2018. All rights reserved.
+  */
+case class CurrentPowerConsumption(kwh: Double)
+
+case class PowerConsumptionStatus(numOfReadings: Long, total: Double, avg: Double, status: String) {
+  def compute(newReadings: List[Double]) = {
+    val newTotal = newReadings.sum + total
+    val newNumOfReadings = numOfReadings + newReadings.size
+    val newAvg = newTotal / newNumOfReadings.toDouble
+
+    PowerConsumptionStatus(newNumOfReadings, newTotal, newAvg, "ON")
+  }
+}
+
+object MultiStreamHandler extends App with KnolXLogger {
+  info("Creating Spark Session")
+  val spark = SparkSession.builder().master(sparkMaster).appName(sparkAppName).getOrCreate()
+  spark.sparkContext.setLogLevel("WARN")
+
+  val updateStateFunc =
+    (deviceId: String, newReadings: Iterator[(String, CurrentPowerConsumption)], state: GroupState[PowerConsumptionStatus]) => {
+      val data = newReadings.toList.map { case(_, reading) => reading }.map(_.kwh)
+
+      lazy val initialPowerConsumptionStatus = PowerConsumptionStatus(0L, 0D, 0D, "OFF")
+      val currentState = state.getOption.fold(initialPowerConsumptionStatus.compute(data))(_.compute(data))
+
+      val currentStatus =
+        if(state.hasTimedOut) {
+          // If we do not receive any reading, for a device, we will assume that it is OFF.
+          currentState.copy(status = "OFF")
+        } else {
+          state.setTimeoutDuration("10 seconds")
+          currentState
+        }
+
+      state.update(currentStatus)
+      (deviceId, currentStatus)
+    }
+
+  info("Creating Streaming DF...")
+  val dataStream =
+    spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", bootstrapServer)
+      .option("subscribe", topic)
+      .option("failOnDataLoss", false)
+      .option("includeTimestamp", true)
+      .load()
+
+  info("Writing data to Console...")
+  import spark.implicits._
+
+  implicit val currentPowerConsumptionEncoder = Encoders.kryo[CurrentPowerConsumption]
+  implicit val powerConsumptionStatusEncoder = Encoders.kryo[PowerConsumptionStatus]
+
+  val query =
+    dataStream
+      .select(col("key").cast(StringType).as("key"), col("value").cast(StringType).as("value"))
+      .as[(String, String)]
+      .map { case(deviceId, unit) =>
+        (deviceId, CurrentPowerConsumption(Option(unit).fold(0D)(_.toDouble)))
+      }
+      .groupByKey { case(deviceId, _) => deviceId }
+      .mapGroupsWithState[PowerConsumptionStatus, (String, PowerConsumptionStatus)](GroupStateTimeout.ProcessingTimeTimeout())(updateStateFunc)
+      .toDF("deviceId", "current_status")
+      .writeStream
+      .format("console")
+      .option("truncate", false)
+      .outputMode(OutputMode.Update())
+      .option("checkpointLocation", checkPointDir)
+      .start()
+
+  info("Waiting for the query to terminate...")
+  query.awaitTermination()
+  query.stop()
+}
